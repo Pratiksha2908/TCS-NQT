83e2ade0d102ce81fa9d98f9faed420b982f4025
Pratiksha Shinde <pratikshashinde2908@outlook.com>
2020-10-12 23:15:01 +0530

    TCS NQT

--- MintingMints.cpp
+++ MintingMints.cpp
@@ -7,9 +7,9 @@ int main()
     int n, len, i, sum, nxtSum=0;
   	cin >> n >> len;
   	for(i=1; i<=len; i++) {
-        sum = nxtSum + n;
-        nxtSum = sum-1;
-        n = sum;
+      sum = nxtSum + n;
+      nxtSum = sum-1;
+      n = sum;
     }
     cout << sum << endl;
 }
